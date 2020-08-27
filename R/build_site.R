487c4fbc24b570b6a3a0c5e9276244ac1cda05f2
Pierre Navaro <pnavaro@users.noreply.github.com>
2020-08-27 17:46:39 +0200

    Update build_site.R

--- R/build_site.R
+++ R/build_site.R
@@ -2,4 +2,4 @@
 lapply(
   list.files('_posts/', recursive = TRUE, pattern = '.Rmd', 
                                   full.names = TRUE), rmarkdown::render)
-rmarkdown::render_site()
+rmarkdown::render_site(encoding = 'UTF-8')
