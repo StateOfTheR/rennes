f0fb433f7bcb9bbc812e1c1d808152da45d714f8
MarieEtienne <MarieEtienne@users.noreply.github.com>
2020-09-28 12:02:46 +0000

    deploy: 12cd86134578a3e29a270772a7fba8287394febf

--- R/build_site.R
+++ R/build_site.R
@@ -1,5 +1,7 @@
+### Given the problems encountered with lapply in xaringan files generation, avoid laply and go for a loop
 
-lapply(
-  list.files('_posts/', recursive = TRUE, pattern = '.Rmd', 
-                                  full.names = TRUE), rmarkdown::render)
+for(f in   list.files('_posts/', recursive = TRUE, pattern = '.Rmd', 
+                      full.names = TRUE))
+  rmarkdown::render(f)
+
 rmarkdown::render_site(encoding = 'UTF-8')
